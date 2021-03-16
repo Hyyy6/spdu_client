55dbd60bc5a533fec5ac7e759b32df671ac711ec
hy6p <roman.tata.b@gmail.com>
2021-03-16 02:09:43 -0700

    Initial commit.

--- src/client.py
+++ src/client.py
@@ -4,14 +4,27 @@ import requests
 
 names = {"pc1": 0, "pc2": 1, "box1": 2, "box3": 3}
 
-def main(argv):
-	for arg in argv:
-		print(arg)
-	print ("main")
 
+
+def printHelp():
+    print("Usage: ./<script> outletName outletState")
+
+def getKey():
+    return
+    
+
+def main(argv):
+	# for arg in argv:
+	# 	print(arg)
+	# print ("main")
+    if argv.size() != 2:
+        print("bad input")
+        print_help()
+    
+    
 
 if __name__ == "__main__":
-	main(sys.argv)
+	main(sys.argv[1:])
 
 print ("hello world")
 # %%
